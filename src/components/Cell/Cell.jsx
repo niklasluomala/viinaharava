f0d4688f40acadb27b0226bfaf8cc6a5db7e8bd0
Niklas Luomala <nemluo@utu.fi>
2024-02-17 14:29:50 +0200

    Rewrite Cell and CellCard as functional

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,48 +1,32 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import "./style.css";
 
-class Cell extends React.Component {
-  getValue() {
-    const { value } = this.props;
+function Cell(props) {
+    const getValue = () => {
+        const { value } = props
 
-    if (value.isMine) {
-      return "💣";
-    } else if (value.isEmpty) {
-      return "";
+        if (value.isMine) {
+            return "💣"
+        } else if (value.isEmpty) {
+            return ""
+        }
+
+        return value.n
     }
 
-    return value.n;
-  }
-
-  render() {
-    const className =
+    const className = 
       "cell" +
-      (this.props.value.isMine ? " is-mine" : "") +
-      (this.props.value.isEmpty ? " is-empty" : "")
+      (props.value.isMine ? " is-mine" : "") +
+      (props.value.isEmpty ? " is-empty" : "")
 
     return (
-      <div
-        className={className}
-      >
-        {this.getValue()}
-      </div>
-    );
-  }
+        <div
+          className={className}
+        >
+            {getValue()}
+        </div>
+    )
 }
-
-// Type checking With PropTypes
-const cellItemShape = {
-  x: PropTypes.number,
-  y: PropTypes.number,
-  n: PropTypes.number,
-  isMine: PropTypes.bool,
-  isEmpty: PropTypes.func
-};
-
-Cell.propTypes = {
-  value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
-};
 
 export default Cell;
--- src/components/CellCard/CellCard.jsx
+++ src/components/CellCard/CellCard.jsx
@@ -1,46 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import "./style.css";
 
-class CellCard extends React.Component {
-  getValue() {
-    const { value } = this.props;
-
-    if (value.isMine) {
-      return "💣";
-    } else if (value.isEmpty) {
-      return "";
-    }
-
-    return value.n;
-  }
-
-  render() {
-    const className =
-      "cellcard"
+function CellCard(props) {
+    const className="cellcard"
 
     return (
-      <div
-        className={className}
-      >
-        <p>{this.props.card}</p>
-      </div>
-    );
-  }
+        <div
+          className={className}
+        >
+            <p>{props.card}</p>
+        </div>
+    )
 }
-
-// Type checking With PropTypes
-const cellItemShape = {
-  x: PropTypes.number,
-  y: PropTypes.number,
-  n: PropTypes.number,
-  isMine: PropTypes.bool,
-};
-
-CellCard.propTypes = {
-  value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
-  card: PropTypes.array
-};
 
 export default CellCard;
