d56036042685ec492e9c062d12de5d6c11169659
Niklas Luomala <nemluo@utu.fi>
2024-02-17 15:00:27 +0200

    Add typescript to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,23 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parser: "@typescript-eslint/parser",
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint'  
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'error'
   },
 }
