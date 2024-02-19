1d59b0bb3bc4968cd73f464e14d016f2d775c14c
Niklas Luomala <nemluo@utu.fi>
2024-02-19 05:57:36 +0200

    Rewrite Game as functional

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,111 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Cell from "../Cell/Cell.jsx";
-import CellCard from "../CellCard/CellCard.jsx";
+import Cell from "../Cell/Cell.tsx";
+import CellCard from "../CellCard/CellCard.tsx";
 
 import "./style.css";
 
 class Board extends React.Component {
-  state = this.getInitialState();
-
-  getInitialState() {
-    const initialState = {
-      grid: this.createNewBoard(),
-      minesCount: this.props.mines,
-      cards: [],
-      mines: [],
-      ones: [],
-      twos: [],
-      threes: [],
-      fours: [],
-      fives: [],
-      sixes: [],
-      sevens: [],
-      eights: [],
-      empties: []
-    };
-    return initialState;
-  }
-
-  // Board utilities
-  createNewBoard() {
-    const grid = [];
-    const rows = this.props.width;
-    const columns = this.props.height;
-    const minesCount = this.props.mines;
-    const minesArray = this.getRandomMines(minesCount, columns, rows);
-
-    for (let i = 0; i < columns; ++i) {
-      grid.push([]);
-      for (let j = 0; j < rows; ++j) {
-        const gridCell = new GridCell(i, j, minesArray.includes(i * rows + j));
-        this.addGridCell(grid, gridCell);
-      }
-    }
-
-    return grid;
-  }
-
-  getRandomMines(amount, columns, rows) {
-    const minesArray = [];
-    const limit = columns * rows;
-    const minesPool = [...Array(limit).keys()];
-
-    for (let i = 0; i < amount; ++i) {
-      const n = Math.floor(Math.random() * minesPool.length);
-      minesArray.push(...minesPool.splice(n, 1));
-    }
-
-    return minesArray;
-  }
-
-  addGridCell(grid, gridCell) {
-    const y = grid.length - 1;
-    const x = grid[y].length;
-    const lastGridCell = gridCell;
-    const neighbours = this.getNeighbours(grid, y, x);
-
-    for (let neighbourGridCell of neighbours) {
-      if (lastGridCell.isMine) {
-        neighbourGridCell.n += 1;
-      } else if (neighbourGridCell.isMine) {
-        lastGridCell.n += 1;
-      }
-    }
-
-    grid[y].push(gridCell);
-  }
-
-  restartBoard() {
-    this.setState(this.getInitialState());
-  }
-
-  /* Helpers */
-  getNeighbours(grid, y, x) {
-    const neighbours = [];
-    const currentRow = grid[y];
-    const prevRow = grid[y - 1];
-    const nextRow = grid[y + 1];
-
-    if (currentRow[x - 1]) neighbours.push(currentRow[x - 1]);
-    if (currentRow[x + 1]) neighbours.push(currentRow[x + 1]);
-    if (prevRow) {
-      if (prevRow[x - 1]) neighbours.push(prevRow[x - 1]);
-      if (prevRow[x]) neighbours.push(prevRow[x]);
-      if (prevRow[x + 1]) neighbours.push(prevRow[x + 1]);
-    }
-    if (nextRow) {
-      if (nextRow[x - 1]) neighbours.push(nextRow[x - 1]);
-      if (nextRow[x]) neighbours.push(nextRow[x]);
-      if (nextRow[x + 1]) neighbours.push(nextRow[x + 1]);
-    }
-
-    return neighbours;
-  }
-
   // Rendering functions
   renderBoard() {
-    const grid = this.state.grid;
+    const grid = this.props.grid
 
     return grid.map(row => {
       const rowCells = row.map(gridCell => (
@@ -115,76 +18,26 @@ class Board extends React.Component {
         />
       ));
 
-      return <div className="row">{rowCells}</div>;
+      return <div className="row" key={row}>{rowCells}</div>;
     });
   }
 
   renderBoardCards() {
-    const grid = this.state.grid;
-
-    // spades, clubs, diamonds, hearts
-    const suites = ["♠️", "♣️", "♦️", "♥️"]
-
-    // card values from A to King
-    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
-    // using 2 decks by default
-    const decks = 2;
-
-    let cards = this.state.cards
-
-    for (let value of values) {
-        for (let suite of suites) {
-            for (let i = 0; i < decks; i++) {
-                this.state.cards.push("".concat(value, suite))
-            }
-        }
-    }
-
-    if (this.state.mines.length === 0) {
-        grid.map(row => {
-            row.map(cell => {
-                if (cell.isMine) {
-                    this.state.mines.push(this.state.cards.splice(cards.findIndex(card => card.match(/[(9)(10)]/g)), 1));
-                } else if (cell.n === 1) {
-                    this.state.ones.push(this.state.cards.splice(cards.findIndex(card => card.match(/[AJ]/g)), 1))
-                } else if (cell.n === 2) {
-                    this.state.twos.push(this.state.cards.splice(cards.findIndex(card => card.match(/[2Q]/g)), 1))
-                } else if (cell.n === 3) {
-                    this.state.threes.push(this.state.cards.splice(cards.findIndex(card => card.match(/[3K]/g)), 1))
-                } else if (cell.n === 4) {
-                    this.state.fours.push(this.state.cards.splice(cards.findIndex(card => card.match(/[4]/g)), 1))
-                } else if (cell.n === 5) {
-                    this.state.fives.push(this.state.cards.splice(cards.findIndex(card => card.match(/[5]/g)), 1))
-                } else if (cell.n === 6) {
-                    this.state.sixes.push(this.state.cards.splice(cards.findIndex(card => card.match(/[6]/g)), 1))
-                } else if (cell.n === 7) {
-                    this.state.sevens.push(this.state.cards.splice(cards.findIndex(card => card.match(/[7]/g)), 1))
-                } else if (cell.n === 8) {
-                    this.state.eights.push(this.state.cards.splice(cards.findIndex(card => card.match(/[8]/g)), 1))
-                } else if (cell.n === 0 && !cell.isMine) {
-                    this.state.empties.push(this.state.cards.splice(cards.findIndex(card => card.match(/[7-8]/g)), 1))
-                }
-            })
-        })
-    }
-
-    return grid.map(row => {
+    return this.props.grid.map(row => {
         const rowCells = row.map(gridCell => (
             <CellCard
               key={gridCell.y * row.length + gridCell.x}
-              value={gridCell}
-              card={this.getCardForCell(gridCell)}
+              gridCell={gridCell}
             />
         ));
 
-        return <div className="rowCard">{rowCells}</div>;
+        return <div className="rowCard" key={ row }>{rowCells}</div>;
     });
   }
 
   renderCards(name, className, arr) {
     let cards = arr.map(card => (
-        <span>{card} </span>
+        <span key={ card }>{ card } </span>
     ));
     if (cards.length == 0) return
     return (
@@ -202,98 +55,36 @@ class Board extends React.Component {
         <div className="grid">{this.renderBoard()}</div>
         <div className="grid">{this.renderBoardCards()}</div>
         <div className="ruleset">
-            <div className="mines">{this.renderCards("Miinat", "mines", this.state.mines)}</div>
-            <div className="ones">{this.renderCards("Ykköset:", "ones", this.state.ones)}</div>
-            <div className="twos">{this.renderCards("Kakkoset:", "twos", this.state.twos)}</div>
-            <div className="threes">{this.renderCards("Kolmoset:", "threes", this.state.threes)}</div>
-            <div className="fours">{this.renderCards("Neloset:", "fours", this.state.fours)}</div>
-            <div className="fives">{this.renderCards("Vitoset:", "fives", this.state.fives)}</div>
-            <div className="sixes">{this.renderCards("Kutoset:", "sixes", this.state.sixes)}</div>
-            <div className="sevens">{this.renderCards("Seiskat:", "sevens", this.state.sevens)}</div>
-            <div className="eights">{this.renderCards("Kasit:", "eights", this.state.eights)}</div>
-            <div className="empties">{this.renderCards("Tyhjät:", "empties", this.state.empties)}</div>
+            <div className="mines">{this.renderCards("Miinat", "mines", this.props.minesCards)}</div>
+            <div className="ones">{this.renderCards("Ykköset:", "ones", this.props.ones)}</div>
+            <div className="twos">{this.renderCards("Kakkoset:", "twos", this.props.twos)}</div>
+            <div className="threes">{this.renderCards("Kolmoset:", "threes", this.props.threes)}</div>
+            <div className="fours">{this.renderCards("Neloset:", "fours", this.props.fours)}</div>
+            <div className="fives">{this.renderCards("Vitoset:", "fives", this.props.fives)}</div>
+            <div className="sixes">{this.renderCards("Kutoset:", "sixes", this.props.sixes)}</div>
+            <div className="sevens">{this.renderCards("Seiskat:", "sevens", this.props.sevens)}</div>
+            <div className="eights">{this.renderCards("Kasit:", "eights", this.props.eights)}</div>
+            <div className="empties">{this.renderCards("Tyhjät:", "empties", this.props.empties)}</div>
         </div>
       </div>
     );
-  }
-
-  getCardForCell(cell) {
-    let card = ""
-    if (cell.isMine) {
-        card = this.state.mines.shift();
-        this.state.mines.push(card);
-    }
-    else if (cell.n === 1) {
-        card = this.state.ones.shift();
-        this.state.ones.push(card);
-    } else if (cell.n === 2) {
-        card = this.state.twos.shift();
-        this.state.twos.push(card);
-    } else if (cell.n === 3) {
-        card = this.state.threes.shift();
-        this.state.threes.push(card);
-    } else if (cell.n === 4) {
-        card = this.state.fours.shift();
-        this.state.fours.push(card);
-    } else if (cell.n === 5) {
-        card = this.state.fives.shift();
-        this.state.fives.push(card);
-    } else if (cell.n === 6) {
-        card = this.state.sixes.shift();
-        this.state.sixes.push(card);
-    } else if (cell.n === 7) {
-        card = this.state.sevens.shift();
-        this.state.sevens.push(card);
-    } else if (cell.n === 8) { 
-        card = this.state.eights.shift();
-        this.state.eights.push(card);
-    } else if (cell.n === 0 && !cell.isMine) {
-        // check if card is already used
-        let cardIsUsed = false;
-        do {
-            cardIsUsed = false;
-            card = this.state.empties.shift();
-            if (this.state.ones.find(one => one == card)) {
-                cardIsUsed = true;
-            } else if (this.state.twos.find(two => two == card)) {
-                cardIsUsed = true;
-            } else if (this.state.threes.find(three => three == card)) {
-                cardIsUsed = true;
-            } else if (this.state.fours.find(four => four == card)) {
-                cardIsUsed = true;
-            } else if (this.state.fives.find(five => five == card)) {
-                cardIsUsed = true;
-            } else if (this.state.sixes.find(six => six == card)) {
-                cardIsUsed = true;
-            } else if (this.state.sevens.find(seven => seven == card)) {
-                cardIsUsed = true;
-            } else if (this.state.eights.find(eight => eight == card)) {
-                cardIsUsed = true;
-            }      
-        } while (cardIsUsed)
-        this.state.empties.push(card);
-    }
-    return card;
-  }
-}
-
-class GridCell {
-  constructor(y, x, isMine) {
-    this.x = x;
-    this.y = y;
-    this.n = 0;
-    this.isMine = isMine;
-  }
-  get isEmpty() {
-    return this.n === 0 && !this.isMine;
   }
 }
 
 // Type checking With PropTypes
 Board.propTypes = {
-  height: PropTypes.number,
-  width: PropTypes.number,
-  mines: PropTypes.number
+  grid: PropTypes.array, 
+  cards: PropTypes.array,
+  minesCards: PropTypes.array,
+  ones: PropTypes.array,
+  twos: PropTypes.array,
+  threes: PropTypes.array,
+  fours: PropTypes.array,
+  fives: PropTypes.array,
+  sixes: PropTypes.array,
+  sevens: PropTypes.array,
+  eights: PropTypes.array,
+  empties: PropTypes.array
 };
 
 export default Board;
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,35 +1,280 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Board from "../Board/Board.jsx";
 
 import "./style.css";
 
 function Game() {
-    const [height, setHeight] = useState(0)
-    const [width, setWidth] = useState(0)
-    const [mines, setMines] = useState(0)
-    
-    setHeight(6)
-    setWidth(6)
-    setMines(8)
-    
-    const restartGame = () => {
-        this.boardElement.current.restartBoard();
-    };
-    
+    const [height, setHeight] = useState(6)
+    const [width, setWidth] = useState(6)
+    const [minesCount, setMinesCount] = useState(8)
+    const [grid, setGrid] = useState([])
+    const [cards, setCards] = useState([])
+    const [minesCards, setMinesCards] = useState([])
+    const [ones, setOnes] = useState([])
+    const [twos, setTwos] = useState([])
+    const [threes, setThrees] = useState([])
+    const [fours, setFours] = useState([])
+    const [fives, setFives] = useState([])
+    const [sixes, setSixes] = useState([])
+    const [sevens, setSevens] = useState([])
+    const [eights, setEights] = useState([])
+    const [empties, setEmpties] = useState([])
+
+    const createNewBoard = () => {
+        let board = []
+        let minesArray = getRandomMines()
+
+        for (let i = 0; i < width; ++i) {
+            board.push([])
+            for (let j = 0; j < height; ++j) {
+                const gridCell = new GridCell(i, j, minesArray.includes(i * height + j))
+                addGridCell(board, gridCell)
+            }
+        }
+
+        return board
+    }
+
+    const cardifyGrid = (board, minesCards, ones, twos, threes, fours, fives, sixes, sevens, eights, empties) => {
+        for (let row of board) {
+            for (let gridCell of row) {
+                let card = getCardForCell(gridCell, minesCards, ones, twos, threes, fours, fives, sixes, sevens, eights, empties)
+                gridCell.card = card
+            }
+        }
+    }
+
+    const getNewDeck = () => {
+       
+        // spades, clubs, diamonds, hearts
+        const suites = ["♠️", "♣️", "♦️", "♥️"]
+
+        // card values from A to King
+        const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+        // using 2 decks by default
+        const decks = 2;
+
+        let deck = []
+
+        for (let value of values) {
+            for (let suite of suites) {
+                for (let i = 0; i < decks; i++) {
+                    deck.push("".concat(value, suite))
+                }
+            }
+        }
+
+        return deck
+    }
+
+    const determineCardsForGrid = (board, minesCards, ones, twos, threes, fours, fives, sixes, sevens, eights, empties) => {
+        board.map(row => {
+            row.map(cell => {
+                if (cell.isMine) {
+                    minesCards.push(cards.splice(cards.findIndex(card => card.match(/[(9)(10)]/g)), 1))
+                } else if (cell.n === 1) {
+                    ones.push(cards.splice(cards.findIndex(card => card.match(/[AJ]/g)), 1))
+                } else if (cell.n === 2) {
+                    twos.push(cards.splice(cards.findIndex(card => card.match(/[2Q]/g)), 1))
+                } else if (cell.n === 3) {
+                    threes.push(cards.splice(cards.findIndex(card => card.match(/[3K]/g)), 1))
+                } else if (cell.n === 4) {
+                    fours.push(cards.splice(cards.findIndex(card => card.match(/[4]/g)), 1))
+                } else if (cell.n === 5) {
+                    fives.push(cards.splice(cards.findIndex(card => card.match(/[5]/g)), 1))
+                } else if (cell.n === 6) {
+                    sixes.push(cards.splice(cards.findIndex(card => card.match(/[6]/g)), 1))
+                } else if (cell.n === 7) {
+                    sevens.push(cards.splice(cards.findIndex(card => card.match(/[7]/g)), 1))
+                } else if (cell.n === 8) {
+                    eights.push(cards.splice(cards.findIndex(card => card.match(/[8]/g)), 1))
+                } else if (cell.n === 0 && !cell.isMine) {
+                    empties.push(cards.splice(cards.findIndex(card => card.match(/[7-8]/g)), 1))
+                }
+            })
+        })
+    }
+
+    const getRandomMines = () => {
+        const minesArray = []
+        const limit = width * height
+        const minesPool = [...Array(limit).keys()]
+
+        for (let i = 0; i < minesCount; ++i) {
+            const n = Math.floor(Math.random() * minesPool.length)
+            minesArray.push(...minesPool.splice(n, 1))
+        }
+
+        return minesArray
+    }
+
+    const addGridCell = (grid, gridCell) => {
+        const y = grid.length - 1
+        const x = grid[y].length
+        const lastGridCell = gridCell
+        const neighbours = getNeighbours(grid, y, x)
+
+        for (let neighbourGridCell of neighbours) {
+            if (lastGridCell.isMine) neighbourGridCell.n += 1
+            else if (neighbourGridCell.isMine) lastGridCell.n += 1
+        }
+
+        grid[y].push(gridCell)
+    }
+
+    const getNeighbours = (grid, y, x) => {
+        const neighbours = []
+        const currentRow = grid[y]
+        const prevRow = grid[y - 1]
+        const nextRow = grid[y + 1]
+
+        if (currentRow[x - 1]) neighbours.push(currentRow[x - 1]);
+        if (currentRow[x + 1]) neighbours.push(currentRow[x + 1]);
+        if (prevRow) {
+            if (prevRow[x - 1]) neighbours.push(prevRow[x - 1]);
+            if (prevRow[x]) neighbours.push(prevRow[x]);
+            if (prevRow[x + 1]) neighbours.push(prevRow[x + 1]);
+        }
+        if (nextRow) {
+            if (nextRow[x - 1]) neighbours.push(nextRow[x - 1]);
+            if (nextRow[x]) neighbours.push(nextRow[x]);
+            if (nextRow[x + 1]) neighbours.push(nextRow[x + 1]);
+        }
+
+        return neighbours;
+    }
+
+    const getCardForCell = (cell, minesCards, ones, twos, threes, fours, fives, sixes, sevens, eights, empties) => {
+        let card = ""
+        if (cell.isMine) {
+            card = minesCards.shift()
+            minesCards.push(card)
+        }
+        else if (cell.n === 1) {
+            card = ones.shift()
+            ones.push(card)
+        } else if (cell.n === 2) {
+            card = twos.shift()
+            twos.push(card)
+        } else if (cell.n === 3) {
+            card = threes.shift();
+            threes.push(card);
+        } else if (cell.n === 4) {
+            card = fours.shift();
+            fours.push(card);
+        } else if (cell.n === 5) {
+            card = fives.shift();
+            fives.push(card);
+        } else if (cell.n === 6) {
+            card = sixes.shift();
+            sixes.push(card);
+        } else if (cell.n === 7) {
+            card = sevens.shift();
+            sevens.push(card);
+        } else if (cell.n === 8) { 
+            card = eights.shift();
+            eights.push(card);
+        } else if (cell.n === 0 && !cell.isMine) {
+            // check if card is already used
+            let cardIsUsed = false;
+            do {
+                cardIsUsed = false;
+                card = empties.shift();
+                if (ones.find(one => one == card)) {
+                    cardIsUsed = true;
+                } else if (twos.find(two => two == card)) {
+                    cardIsUsed = true;
+                } else if (threes.find(three => three == card)) {
+                    cardIsUsed = true;
+                } else if (fours.find(four => four == card)) {
+                    cardIsUsed = true;
+                } else if (fives.find(five => five == card)) {
+                    cardIsUsed = true;
+                } else if (sixes.find(six => six == card)) {
+                    cardIsUsed = true;
+                } else if (sevens.find(seven => seven == card)) {
+                    cardIsUsed = true;
+                } else if (eights.find(eight => eight == card)) {
+                    cardIsUsed = true;
+                }      
+            } while (cardIsUsed)
+            empties.push(card);
+        }
+        return card;
+      }
+
+    const createNewGame = () => {
+        let board = createNewBoard()
+        let deck = getNewDeck()
+        let minesDeck = []
+        let onesDeck = []
+        let twosDeck = []
+        let threesDeck = []
+        let foursDeck = []
+        let fivesDeck = []
+        let sixesDeck = []
+        let sevensDeck = []
+        let eightsDeck = []
+        let emptiesDeck = []
+
+        determineCardsForGrid(board, minesDeck, onesDeck, twosDeck, threesDeck, foursDeck, fivesDeck, sixesDeck,
+            sevensDeck, eightsDeck, emptiesDeck)
+        cardifyGrid(board, minesDeck, onesDeck, twosDeck, threesDeck, foursDeck, fivesDeck, sixesDeck,
+            sevensDeck, eightsDeck, emptiesDeck)
+        setGrid(board)
+        setCards(deck)
+        setMinesCards(minesDeck)
+        setOnes(onesDeck)
+        setTwos(twosDeck)
+        setThrees(threesDeck)
+        setFours(foursDeck)
+        setFives(fivesDeck)
+        setSixes(sixesDeck)
+        setSevens(sevensDeck)
+        setEights(eightsDeck)
+        setEmpties(emptiesDeck)
+    }  
+
+    useEffect(() => {
+        createNewGame()
+    }, [])
+
     return (
     <div className="game">
     <div className="control-buttons">
-    <button onClick={restartGame()}>Generoi</button>
+    <button onClick={() => createNewGame()}>Generoi</button>
     </div>
         <br />
         <Board
-        ref={this.boardElement}
-        height={height}
-        width={width}
-        mines={mines}
+            grid={grid}
+            cards={cards}
+            minesCards={minesCards}
+            ones={ones}
+            twos={twos}
+            threes={threes}
+            fours={fours}
+            fives={fives}
+            sixes={sixes}
+            sevens={sevens}
+            eights={eights}
+            empties={empties}
         />
         </div>
     )
+}
+
+class GridCell {
+    constructor (y, x, isMine, card) {
+        this.x = x
+        this.y = y
+        this.card = card
+        this.n = 0
+        this.isMine = isMine
+    }
+    get isEmpty() {
+        return this.n === 0 && !this.isMine
+    }
 }
     
 export default Game;
