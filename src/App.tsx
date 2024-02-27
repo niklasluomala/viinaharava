ce5319eefbe7913db1a0cc8a6a834414210f7273
Niklas Luomala <nemluo@utu.fi>
2024-02-27 22:31:01 +0200

    Start implementing Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import Game from './components/Game/Game.jsx';
 import RulesMarkdown from './rules.md';
 import ThanksMarkdown from './thanks.md';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import React from 'react';
+import * as React from 'react';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Box from '@mui/material/Box';
+import Markdown from 'mui-markdown';
 
 function App() {
   const [rules, setRules] = useState('');
   const [thanks, setThanks] = useState('');
+  const [value, setValue] = useState('1');
+
+  const handleChange = (e: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     fetch(RulesMarkdown)
@@ -25,22 +33,26 @@ function App() {
   return (
     <>
       <div>
-        <Tabs>
-          <TabList>
-            <Tab>Säännöt</Tab>
-            <Tab>Generaattori</Tab>
-            <Tab>Kiitokset</Tab>
-          </TabList>
-          <TabPanel>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{rules}</ReactMarkdown>
-          </TabPanel>
-          <TabPanel>
-            <Game />
-          </TabPanel>
-          <TabPanel>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>{thanks}</ReactMarkdown>
-          </TabPanel>
-        </Tabs>
+        <Box sx={{ width: '100%', typography: 'body1' }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <TabList onChange={handleChange} aria-label="lab API tabs example" centered>
+                <Tab label="Säännöt" value="1" />
+                <Tab label="Generaattori" value="2" />
+                <Tab label="Kiitokset" value="3" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <Markdown>{rules}</Markdown>
+            </TabPanel>
+            <TabPanel value="2">
+              <Game />
+            </TabPanel>
+            <TabPanel value="3">
+              <Markdown>{thanks}</Markdown>
+            </TabPanel>
+          </TabContext>
+        </Box>
       </div>
     </>
   );
