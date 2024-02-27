5e30520f4d3ee85d2e430d0a52919ce7a586fba5
Konsta Purtsi <konsta@arado.fi>
2024-02-27 15:52:23 +0200

    chore: install and run Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
-import 'react-tabs/style/react-tabs.css'
-import Game from './components/Game/Game.jsx'
-import RulesMarkdown from './rules.md'
-import ThanksMarkdown from './thanks.md'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import React from 'react'
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Game from './components/Game/Game.jsx';
+import RulesMarkdown from './rules.md';
+import ThanksMarkdown from './thanks.md';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import React from 'react';
 
 function App() {
-  const [rules, setRules] = useState("")
-  const [thanks, setThanks] = useState("")
+  const [rules, setRules] = useState('');
+  const [thanks, setThanks] = useState('');
 
   useEffect(() => {
     fetch(RulesMarkdown)
-      .then(response => response.text())
-      .then(text => setRules(text))
+      .then((response) => response.text())
+      .then((text) => setRules(text));
     fetch(ThanksMarkdown)
-      .then(response => response.text())
-      .then(text => setThanks(text))
-  }, [])
+      .then((response) => response.text())
+      .then((text) => setThanks(text));
+  }, []);
 
   return (
     <>
@@ -32,22 +32,18 @@ function App() {
             <Tab>Kiitokset</Tab>
           </TabList>
           <TabPanel>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {rules}
-            </ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{rules}</ReactMarkdown>
           </TabPanel>
           <TabPanel>
             <Game />
           </TabPanel>
           <TabPanel>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-              {thanks}
-            </ReactMarkdown>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{thanks}</ReactMarkdown>
           </TabPanel>
         </Tabs>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
